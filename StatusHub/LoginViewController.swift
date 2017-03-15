b6b97f2338c0efdbcffc3810556832c9c70768f2
Subodh Jena <jenasubodh@gmail.com>
2017-03-16 03:53:56 +0530

    Removing Registration For Now
    Removing Registration For Now

--- StatusHub/LoginViewController.swift
+++ StatusHub/LoginViewController.swift
@@ -15,8 +15,6 @@ class LoginViewController: UIViewController {
     
     @IBOutlet weak var txtEmail: UITextField!
     @IBOutlet weak var txtPassword: UITextField!
-    @IBOutlet weak var txtName: UITextField!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -81,17 +79,7 @@ class LoginViewController: UIViewController {
         }
     }
     
-    @IBAction func didTapRegister(_ sender: Any) {
-        if(isRegistrationFormValid()){
-            dismissLogin()
-        }
-    }
-    
     func isLoginFormValid() -> Bool {
-        return true
-    }
-    
-    func isRegistrationFormValid() -> Bool {
         return true
     }
     
